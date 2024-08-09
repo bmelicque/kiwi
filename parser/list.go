c9c2f7aa555baa1797fabee7e4c8cae4ff5b364c
bmelicque <bmelicque@gmail.com>
2024-08-09 17:44:45 +0200

    list

--- parser/list.go
+++ parser/list.go
@@ -5,39 +5,12 @@ import (
 )
 
 type ListExpression struct {
-	Elements []Expression
+	Elements []Node
 	loc      tokenizer.Loc
 }
 
-func (l ListExpression) Type() ExpressionType {
-	if len(l.Elements) == 0 {
-		return List{Primitive{UNKNOWN}}
-	}
-	t := l.Elements[0].Type()
-	if t.Kind() == TYPE {
-		return t
-	}
-	return List{t}
-}
-
-func (l ListExpression) Check(c *Checker) {
-	var t ExpressionType
-	for _, el := range l.Elements {
-		if el == nil {
-			continue
-		}
-		el.Check(c)
-		if t == nil {
-			t = el.Type()
-		} else if !t.Extends(el.Type()) {
-			c.report("Types don't match", el.Loc())
-		}
-	}
-
-}
-
 func (l ListExpression) Loc() tokenizer.Loc { return l.loc }
-func (l ListExpression) Parse(p *Parser) Expression {
+func (l ListExpression) Parse(p *Parser) Node {
 	lbracket := p.tokenizer.Consume()
 	l.loc.Start = lbracket.Loc().Start
 
