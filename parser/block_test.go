7a4382b2e173ddebae4598fcbe3fff2b3ef97be4
bmelicque <bmelicque@gmail.com>
2024-10-10 11:07:53 +0200

    [broken] move block tests to parser

--- parser/block_test.go
+++ parser/block_test.go
@@ -8,10 +8,13 @@ func TestEmptyBlock(t *testing.T) {
 		token{kind: RightBrace},
 	}}
 	parser := MakeParser(&tokenizer)
-	parser.parseBlock()
+	block := parser.parseBlock()
 
 	if len(parser.errors) != 0 {
 		t.Fatalf("Expected no errors, got %#v", parser.errors)
+	}
+	if block.Type().Kind() != NIL {
+		t.Fatalf("Expected nil type, got %#v", block.Type())
 	}
 }
 
@@ -22,10 +25,13 @@ func TestSingleLineBlock(t *testing.T) {
 		token{kind: RightBrace},
 	}}
 	parser := MakeParser(&tokenizer)
-	parser.parseBlock()
+	block := parser.parseBlock()
 
 	if len(parser.errors) != 0 {
 		t.Fatalf("Expected no errors, got %#v", parser.errors)
+	}
+	if block.Type().Kind() != STRING {
+		t.Fatalf("Expected string type, got %#v", block.Type())
 	}
 }
 
@@ -51,5 +57,31 @@ func TestMultilineBlock(t *testing.T) {
 	}
 	if len(block.Statements) != 2 {
 		t.Fatalf("Expected 2 statements, got %#v", block.Statements)
+	}
+	if block.Type().Kind() != STRING {
+		t.Fatalf("Expected string type, got %#v", block.Type())
+	}
+}
+
+func TestUnreachableCode(t *testing.T) {
+	tokenizer := testTokenizer{tokens: []Token{
+		token{kind: LeftBrace},
+		token{kind: EOL},
+
+		token{kind: ReturnKeyword},
+		literal{kind: StringLiteral, value: "Hello, world!"},
+		token{kind: EOL},
+
+		literal{kind: StringLiteral, value: "Hello, world!"},
+		token{kind: EOL},
+
+		token{kind: RightBrace},
+		token{kind: EOL},
+	}}
+	parser := MakeParser(&tokenizer)
+	parser.parseBlock()
+
+	if len(parser.errors) != 1 {
+		t.Fatalf("Expected 1 error, got %#v", parser.errors)
 	}
 }
