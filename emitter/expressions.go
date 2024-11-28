009c381ce38e145e396a45bcc406b1d0f4ee4ea9
bmelicque <bmelicque@gmail.com>
2024-11-28 16:48:01 +0100

    move binary

--- emitter/expressions.go
+++ emitter/expressions.go
@@ -6,35 +6,6 @@ import (
 	"github.com/bmelicque/test-parser/parser"
 )
 
-func (e *Emitter) emitBinaryExpression(expr *parser.BinaryExpression) {
-	precedence := Precedence(expr)
-	if expr.Left != nil {
-		left := Precedence(expr.Left)
-		if left < precedence {
-			e.write("(")
-		}
-		e.emit(expr.Left)
-		if left < precedence {
-			e.write(")")
-		}
-	}
-
-	e.write(" ")
-	e.write(expr.Operator.Text())
-	e.write(" ")
-
-	if expr.Right != nil {
-		right := Precedence(expr.Right)
-		if right < precedence {
-			e.write("(")
-		}
-		e.emit(expr.Right)
-		if right < precedence {
-			e.write(")")
-		}
-	}
-}
-
 func (e *Emitter) emitBlockExpression(b *parser.Block) {
 	if id, ok := e.uninlinables[b]; ok {
 		e.write(fmt.Sprintf("_tmp%v", id))
