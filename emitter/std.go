8ca09170b955b001ac54ac2895cdabc3be3ccb7a
bmelicque <bmelicque@gmail.com>
2025-01-08 17:44:05 +0100

    emit core js

---
+++ emitter/std.go
@@ -0,0 +1,52 @@
+package emitter
+
+import (
+	"errors"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func createRandomString(length int) string {
+	bytes := make([]byte, length)
+	for i := range bytes {
+		bytes[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(bytes)
+}
+func fileDoesNotExists(path string) bool {
+	_, err := os.Stat(path)
+	return errors.Is(err, os.ErrNotExist)
+}
+func createStdName(rootDir string) string {
+	for {
+		name := createRandomString(8) + ".js"
+		if fileDoesNotExists(filepath.Join(rootDir, createRandomString(8))) {
+			return name
+		}
+	}
+}
+
+// returns the path to the .js file containing std lib
+func EmitStd(rootDir, outDir string) string {
+	name := createStdName(rootDir)
+	f, err := os.Create(filepath.Join(outDir, name))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// sum types
+	f.WriteString("export function Sum(t,v){this.tag=t;if(arguments.length>1)this.value=v}\n")
+
+	// method for handling dom Node getter properties
+	f.WriteString("export let callNodeGetter=(o,m)=>typeof o[m]==\"function\"?o[m]():o[m]\n")
+	f.WriteString("export let bindNodeGetter=(o,m)=>typeof o[m]==\"function\"?o[m].bind(o):()=>o[m]\n")
+
+	// creating "pointers"
+	f.WriteString("export class Pointer{constructor(c,n){this.c=c;this.n=n}get(){return this.c[this.n]}set(v){this.c[this.n]=v}}\n")
+
+	return name
+}
--- main.go
+++ main.go
@@ -35,6 +35,7 @@ type chunk struct {
 
 func transpile(rootPath string, outDir string) {
 	outDir = filepath.Dir(outDir)
+	emptyOutDir(outDir)
 	files, _ := parser.GetCompileOrder(rootPath)
 
 	chunks := []chunk{}
@@ -53,8 +54,9 @@ func transpile(rootPath string, outDir string) {
 		return
 	}
 
+	std := emitter.EmitStd(filepath.Dir(rootPath), outDir)
 	for _, chunk := range chunks {
-		writeChunk(chunk)
+		writeChunk(chunk, std)
 	}
 }
 
@@ -65,13 +67,15 @@ func getOutPath(rootPath, filePath, outDir string) string {
 	return outFile[:len(outFile)-ext] + ".js"
 }
 
-func writeChunk(chunk chunk) {
+func writeChunk(chunk chunk, stdName string) {
 	f, err := os.Create(chunk.path)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
+	stdName = stdName[:len(stdName)-3]
+	f.WriteString("import * as __ from \"" + stdName + "\";\n")
 	_, err = f.WriteString(emitter.EmitProgram(chunk.nodes))
 	if err != nil {
 		log.Fatal(err)
@@ -85,5 +89,24 @@ func logErrors(errors []parser.ParserError) {
 		col := err.Node.Loc().Start.Col
 		msg := err.Text()
 		fmt.Printf("Error at line %v, col. %v: %v\n", line, col, msg)
+	}
+}
+
+func emptyOutDir(dir string) {
+	d, err := os.Open(dir)
+	if err != nil {
+		panic(err)
+	}
+	defer d.Close()
+
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		panic(err)
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			panic(err)
+		}
 	}
 }
